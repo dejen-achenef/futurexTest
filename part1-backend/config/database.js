36e4b29c3c9001cb373f163232923a80bf309213
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2025-12-01 01:02:04 +0300

    adding db host from railway

--- part1-backend/config/database.js
+++ part1-backend/config/database.js
@@ -1,28 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'futurex_db',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    logging: console.log
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "futurex_db",
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    logging: console.log,
   },
   test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME + '_test' || 'futurex_db_test',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql'
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME + "_test" || "futurex_db_test",
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
-  }
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+  },
 };
-
